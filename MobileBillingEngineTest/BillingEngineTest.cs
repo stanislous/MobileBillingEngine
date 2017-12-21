1292610e99c35f4780964bbecaf5bb3ee8580ae4
sva <sva@TI-NB-095>
2017-12-21 16:13:14 +0530

    Add project files.

---
+++ MobileBillingEngineTest/BillingEngineTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NUnit.Framework;
+using 
+
+namespace MobileBillingEngineTest
+{
+    [TestFixture]
+    public class BillingEngineTest
+    {
+        private BillingEngineImp _sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _sut = new NumberToWords();
+        }
+    }
+}
