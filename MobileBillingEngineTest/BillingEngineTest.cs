a84374d28811bf71f62e2e6df86d102e436230eb
stanislous <stanislousvanderputt@gmail.com>
2017-12-22 09:36:49 +0530

    Wrote tests for Call Details Record Class

--- MobileBillingEngineTest/BillingEngineTest.cs
+++ MobileBillingEngineTest/BillingEngineTest.cs
@@ -8,31 +8,59 @@ namespace MobileBillingEngineTest
     public class BillingEngineTest
     {
         private BillingEngingImp bengine_sut;
-        private CallDetailRecordsImp cdr_sut; 
+        private CallDetailRecordsImp cdr_sut;
 
         [SetUp]
         public void Setup()
         {
             bengine_sut = new BillingEngine();
             cdr_sut = new CallDetailRecords();
-
-            cdr_sut.setCallingParty("077-3082022");
-            cdr_sut.setRecievingParty("071-123456");
-            cdr_sut.setStartingTime(new DateTime(2017, 3, 24, 8, 15, 0));
-            cdr_sut.setCallDuration(120);
-            
         }
         [Test]
-        public void SetCallDetsilsRecord_something_ReturnSameDetails()
+        public void SetCallingPartyNumber_something_ReturnSameDetails()
         {
             //arrange
-
+            var expected = "077-3082022";
+            cdr_sut.setCallingParty(expected);
 
             //act
-
+            var result = cdr_sut.getCallingParty();
 
             //assert
-           
+            Assert.AreEqual(expected, result, "Should return same phone number.");
         }
+        [Test]
+        public void SetRecievingPartyNumber_something_ReturnSameDetails()
+        {
+            //arrange
+            var expected = "071-123456";
+            cdr_sut.setRecievingParty(expected);
+
+            //act
+            var result = cdr_sut.getRecievingParty();
+
+            //assert
+            Assert.AreEqual(expected, result, "Should return same phone number.");
+        }
+        [Test]
+        public void SetStartingTimeAndDuration_something_ReturnSameDetails()
+        {
+            //arrange
+            var expected_date_time = new DateTime(2017, 3, 24, 8, 15, 0);
+            var expected_call_duration = 120;
+            cdr_sut.setStartingTime(expected_date_time);
+            cdr_sut.setCallDuration(expected_call_duration);
+
+            //act
+            var result_date_time = cdr_sut.getStartingTime();
+            var result_call_duration = cdr_sut.getCallDuration();
+
+            //assert
+            Assert.AreEqual(expected_date_time, result_date_time, "Should return same date time.");
+            Assert.AreEqual(expected_call_duration,result_call_duration, "Should return same call duration.");
+        }
+
     }
 }
+            
+            
